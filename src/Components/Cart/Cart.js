63cf7d31c6f87d707a2b071642af93a1ddf03e49
mohibuslc <mohib_uslc@outlook.com>
2021-06-27 22:29:32 +0100

    GrandTotal

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -10,11 +10,33 @@ for (let  i=0; i<cart.length; i++){
     total=total+prd.price;
 
 }
+let shipping = 0;
+
+if (total >35){
+
+    shipping=0;
+
+}
+else if (total >15){
+    shipping=4.99
+}
+else if ( total >0){
+    shipping =12.99
+    
+}
+
+
+const tax = Math.round(total/10);
+
+const grandtotal =(total+shipping+tax);
     return (
         <div>
             <h2>Order Summery</h2>
-            <p>Iteam Length :{cart.length}</p>
+            <p>Iteam Length :  {cart.length}</p>
             <p>Total Price: {total}</p>
+            <p><small> Tax + Vat : {tax}</small></p>
+            <p>Shipping Cost : {shipping}</p>
+            <p>Total Price + Shipping Cost: {grandtotal}</p>
         </div>
     );
 };
